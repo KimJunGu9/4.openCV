5d7bb3384a22a4a767f6effb1bd9489ac0ec02c0
KimJunGu9 <133904419+KimJunGu9@users.noreply.github.com>
2023-05-31 01:08:00 +0900

    4-2.arithmetic.py

--- 4-2.arithmetic.py
+++ 4-2.arithmetic.py
@@ -17,6 +17,8 @@ dst3 = cv2.absdiff(src1, src2)
 
 plt.subplot(221), plt.axis('off'), plt.imshow(src1, 'gray'), plt.title('src1')
 plt.subplot(222), plt.axis('off'), plt.imshow(dst1, 'gray'), plt.title('add')
+# 흰색은 어디를 더하든 흰색이다.
 plt.subplot(223), plt.axis('off'), plt.imshow(dst2, 'gray'), plt.title('subtract')
+# 전체에서 흰색을 빼면 검은색이 나온다.
 plt.subplot(224), plt.axis('off'), plt.imshow(dst3, 'gray'), plt.title('absdiff')
 plt.show()
